e08dd601ef99ad07c52cfca71c1d207eb1dfe3e8
imReset <fffffreinds101@gmail.com>
2022-02-13 15:25:49 -0800

    add actionjs

--- components/modal/TransferModal.js
+++ components/modal/TransferModal.js
@@ -1,7 +1,61 @@
-import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import Transfer from "./Transfer";
+import CoinSelector from "./CoinSelector";
+import { TailSpin } from "react-loader-spinner";
+import Recieve from "./Recieve";
 
-function TransferModal() {
-  return <div>TransferModal</div>;
+function TransferModal({ sanityTokens, thirdWebTokens, walletAdress }) {
+  const [action, setAction] = useState("send");
+  const [selectedToken, setSelectedToken] = useState(sanityTokens[0]);
+
+  const selectedStyle = {
+    color: "#3773f5",
+  };
+  const unselectedStyle = {
+    border: "1px solid #282b2f",
+  };
+
+  const selectedModal = (option) => {
+    switch (option) {
+      case "send":
+        return (
+          <Transfer
+            selectedToken={selectedToken}
+            setAction={setAction}
+            thirdWebTokens={thirdWebTokens}
+            walletAdress={walletAdress}
+          />
+        );
+      case "recieve":
+        return (
+          <Recieve
+            setAction={setAction}
+            selectedToken={selectedToken}
+            walletAdress={walletAdress}
+          />
+        );
+      case "select":
+        return (
+          <CoinSelector
+            setAction={setAction}
+            selectedToken={selectedToken}
+            setSelectedToken={setSelectedToken}
+            sanityTokens={sanityTokens}
+            thirdWebTokens={thirdWebTokens}
+            walletAdress={walletAdress}
+          />
+        );
+        case "transferring":
+          retunr (
+            <div
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              
+            }}
+          )
+    }
+  };
 }
-
-export default TransferModal;
