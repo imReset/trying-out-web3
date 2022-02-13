d80beed5425f29960037c8289ad83d52e484e8b2
imReset <fffffreinds101@gmail.com>
2022-02-13 15:39:17 -0800

    finish

--- components/modal/TransferModal.js
+++ components/modal/TransferModal.js
@@ -111,3 +111,38 @@ function TransferModal({ sanityTokens, thirdWebTokens, walletAdress }) {
 }
 
 export default TransferModal;
+
+const Wrapper = styled.div`
+  height: 35rem;
+  width: 27rem;
+  color: white;
+  border: 1px solid #282b2f;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Selector = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 5rem;
+`;
+
+const Option = styled.div`
+  height: 100%;
+  width: 100%;
+  display: grid;
+  place-items: center;
+  font-size: 1.2rem;
+  font-weight: 600;
+
+  &:hover {
+    cursor: PointerEvent;
+    background-color: #111214;
+  }
+`;
+
+const ModalMain = styled.div`
+padding: 1rem;
+flex: 1;
+`;
