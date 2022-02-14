4f9c5746b257fb6a83d3c13bffd0b2d1e89b4fd1
imReset <fffffreinds101@gmail.com>
2022-02-13 19:58:53 -0800

    creae wallet address

--- components/modal/CoinSelector.js
+++ components/modal/CoinSelector.js
@@ -14,3 +14,40 @@ function Receive({ setAction, selectedToken, walletAddress }) {
     setImageUrl(url);
   }, [selectedToken]);
 }
+
+return (
+  <Wrapper>
+    <Content>
+      <QRContainer>
+        <img
+          src={`https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${walletAddress}`}
+          alt=""
+        />
+      </QRContainer>
+      <Divider />
+      <Row>
+        <CoinSelectList>
+          <Icon>
+            <img src={imageUrl} alt={selectedToken.name} />
+          </Icon>
+          <CoinName>{selectedToken.name}</CoinName>
+        </CoinSelectList>
+      </Row>
+      <Divider />
+      <Row>
+        <div>
+          <Title>{selectedToken.symbol} Address</Title>
+          <Address>{walletAddress}</Address>
+        </div>
+        <CopyButton
+          onClick={() => {
+            navigator.clipboard.writeText(walletAddress);
+            setCopied(true);
+          }}
+        >
+          {copied ? <FaCheck style={{ color: "#27ad75" }} /> : <BiCopy />}
+        </CopyButton>
+      </Row>
+    </Content>
+  </Wrapper>
+);
