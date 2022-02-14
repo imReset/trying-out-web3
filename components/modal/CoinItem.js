6ff169b79b3a86721b023c668c3d1a63f4da1f61
imReset <fffffreinds101@gmail.com>
2022-02-13 20:24:53 -0800

    update

--- components/modal/CoinItem.js
+++ components/modal/CoinItem.js
@@ -3,3 +3,38 @@ import styled from "styled-components";
 import imageUrlBuilder from "@sanity/image-url";
 import { client } from "../../lib/sanity";
 import { FaCheck } from "react-icons/fa";
+
+function CoinItem({
+  token,
+  sender,
+  selectedToken,
+  setSelectedToken,
+  thirdWebTokens,
+  sanityTokens,
+  setAction,
+}) {
+  const [balance, setBalance] = useState(0);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    const getBalance = async () => {
+      let activeThirdWebToken;
+
+      thirdWebTokens.map((thirdWebToken) => {
+        if (thirdWebToken.address === token.contractAddress) {
+          activeThirdWebToken = thirdWebToken;
+        }
+      });
+      const balance = await activeThirdWebToken.balanceOf(sender);
+      return await setBalance(balance.displayValue.split(".")[0]);
+    };
+
+    const getImageUrl = async () => {
+      const imgUrl = imageUrlBuilder(client).image(token.logo).url();
+      setImageUrl(imgUrl);
+    };
+
+    getImageUrl();
+    getBalance();
+  }, []);
+}
