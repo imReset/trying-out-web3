362233d4f6828a3741a2e3da908a18f76a68c98f
imReset <fffffreinds101@gmail.com>
2022-02-13 20:36:20 -0800

    add styling

--- components/modal/CoinItem.js
+++ components/modal/CoinItem.js
@@ -37,31 +37,77 @@ function CoinItem({
     getImageUrl();
     getBalance();
   }, []);
+
+  return (
+    <Wrapper
+      style={{
+        backgroundColor: selectedToken.name === token.name && "#141519",
+      }}
+      onClick={() => setSelectedToken(token)}
+    >
+      <Main>
+        <Icon>
+          <img src={imageUrl} alt={token.name} />
+        </Icon>
+        <NameDetails>
+          <Name>{token.name}</Name>
+          <Symbol>{token.symbol}</Symbol>
+        </NameDetails>
+      </Main>
+      <Balance>
+        {balance} {token.symbol}
+      </Balance>
+      <IsSelected>
+        {selectedToken.contractAddress === token.contractAddress && <FaCheck />}
+      </IsSelected>
+    </Wrapper>
+  );
 }
 
-return (
-  <Wrapper
-    style={{
-      backgroundColor: selectedToken.name === token.name && "#141519",
-    }}
-    onClick={() => setSelectedToken(token)}
-  >
-    <Main>
-      <Icon>
-        <img src={imageUrl} alt={token.name} />
-      </Icon>
-      <NameDetails>
-        <Name>{token.name}</Name>
-        <Symbol>{token.symbol}</Symbol>
-      </NameDetails>
-    </Main>
-    <Balance>
-      {balance} {token.symbol}
-    </Balance>
-    <IsSelected>
-      {selectedToken.contractAddress === token.contractAddress && <FaCheck />}
-    </IsSelected>
-  </Wrapper>
-);
-
 export default CoinItem;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 1rem 0.5rem;
+  border-radius: 0.5rem;
+  margin-bottom: 0.3rem;
+  &:hover {
+    background-color: #0e0f14;
+  }
+`;
+
+const Main = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Icon = styled.div`
+  margin-right: 1rem;
+  height: 1.8rem;
+  width: 1.8rem;
+  border-radius: 50%;
+  overflow: hidden;
+  display: grid;
+  place-items: center;
+  & > img {
+    height: 120%;
+    width: 120%;
+    object-fit: cover;
+  }
+`;
+
+const NameDetails = styled.div``;
+
+const Name = styled.div`
+  font-size: 1.1rem;
+  margin-bottom: 0.2rem;
+`;
+
+const Balance = styled.div``;
+
+const IsSelected = styled.div`
+  color: #888f9b;
+  font-size: 0.8rem;
+`;
