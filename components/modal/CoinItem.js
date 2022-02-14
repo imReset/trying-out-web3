0b4345d13f751391689243a75bffe380f81f8340
imReset <fffffreinds101@gmail.com>
2022-02-13 20:20:23 -0800

    import

--- components/modal/CoinItem.js
+++ components/modal/CoinItem.js
@@ -1,3 +1,5 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import imageUrlBuilder from "@sanity/image-url";
+import { client } from "../../lib/sanity";
+import { FaCheck } from "react-icons/fa";
