f9fbdc48086cb9ff478f78039555286421895061
imReset <fffffreinds101@gmail.com>
2022-02-13 18:23:22 -0800

    add styling

--- components/modal/Transfer.js
+++ components/modal/Transfer.js
@@ -98,3 +98,122 @@ function Transfer({ selectedToken, setAction, thirdWebTokens, walletAddress }) {
     </Wrapper>
   );
 }
+
+export default Transfer;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  flex: 1;
+`;
+
+const Amount = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const FlexInputContainer = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: flex-end;
+  & > span {
+    font-size: 3rem;
+    margin-bottom: 0.5rem;
+    color: #3773f5;
+  }
+`;
+
+const FlexInput = styled.input`
+  border: none;
+  background: none;
+  outline: none;
+  color: white;
+  font-size: 1.2rem;
+  text-wrap: wrap;
+  text-align: right;
+  max-width: 45%;
+  margin-right: 1rem;
+  font-size: 4.5rem;
+  color: #3773f5;
+  &::-webkit-outer-spin-button,
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+`;
+
+const Warning = styled.div`
+  padding: 1rem 0 2rem 0;
+  text-align: center;
+  color: ${(p) => (p.amount ? `#0a0b0d;` : "#8a919e;")};
+`;
+
+const TransferForm = styled.div`
+  border: 1px solid #282b2f;
+  border-radius: 0.4rem;
+`;
+
+const Divider = styled.div`
+  border-bottom: 1px solid #282b2f;
+`;
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #8a919e;
+  padding: 1rem 0;
+  font-size: 1.2rem;
+`;
+
+const FieldName = styled.div`
+  flex: 0.5;
+  padding-left: 2rem;
+`;
+
+const Icon = styled.div`
+  margin-right: 1rem;
+  height: 1.8rem;
+  width: 1.8rem;
+  border-radius: 50%;
+  overflow: hidden;
+  display: grid;
+  place-items: center;
+  & > img {
+    height: 120%;
+    width: 120%;
+    object-fit: cover;
+  }
+`;
+
+const Recipient = styled.div`
+  flex: 1;
+  border: none;
+  background: none;
+  outline: none;
+  color: white;
+  font-size: 1.2rem;
+  text-wrap: wrap;
+  margin-right: 0.5rem;
+`;
+
+const CoinSelectList = styled.div`
+  display: flex;
+  flex: 1.3;
+  height: 100%;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const CoinName = styled.div`
+  flex: 1;
+  border: none;
+  background: none;
+  outline: none;
+  color: white;
+  font-size: 1.2rem;
+  text-wrap: wrap;
+  margin-right: 0.5rem;
+`;
