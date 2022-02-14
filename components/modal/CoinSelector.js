c1e2db15aa68f68d27647b18f369f52ee6da4ef7
imReset <fffffreinds101@gmail.com>
2022-02-13 19:54:59 -0800

    add function

--- components/modal/CoinSelector.js
+++ components/modal/CoinSelector.js
@@ -4,3 +4,13 @@ import imageUrlBuilder from "@sanity/image-url";
 import { client } from "../../lib/sanity";
 import { BiCopy } from "react-icons/bi";
 import { FaCheck } from "react-icons/fa";
+
+function Receive({ setAction, selectedToken, walletAddress }) {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    const url = imageUrlBuilder(client).image(selectedToken.logo).url();
+    setImageUrl(url);
+  }, [selectedToken]);
+}
