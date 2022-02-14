8b3d2242687c3c5af3578271e712b39da66e17db
imReset <fffffreinds101@gmail.com>
2022-02-13 19:50:07 -0800

    import

--- components/modal/CoinSelector.js
+++ components/modal/CoinSelector.js
@@ -1,7 +1,6 @@
-import React from "react";
-
-function CoinSelector() {
-  return <div>CoinSelector</div>;
-}
-
-export default CoinSelector;
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import imageUrlBuilder from "@sanity/image-url";
+import { client } from "../../lib/sanity";
+import { BiCopy } from "react-icons/bi";
+import { FaCheck } from "react-icons/fa";
