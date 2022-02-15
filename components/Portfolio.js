9df14afe2bc0a39b4dc0ba3312c5bc262d040116
imReset <fffffreinds101@gmail.com>
2022-02-14 19:29:01 -0800

    add portfolio

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,7 +1,15 @@
-import React from "react";
+import styled from "styled-components";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { coins } from "../data/coins";
+import Coin from "./Coin";
+import BalanceChart from "./BalanceChart";
+import { useEffect, useState } from "react";
 
-function Portfolio() {
-  return <div>Portfolio</div>;
+function Portfolio({ thirdWebTokens, sanityTokens, walletAddress }) {
+  const [walletBalance, setWalletBalance] = useState(0);
+
+  const tokenToUsd = {};
+  for (const token of sanityTokens) {
+    tokenToUsd[token.contractAddress] = Number(token.usdPrice);
+  }
 }
-
-export default Portfolio;
