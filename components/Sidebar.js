005e9cea36015cb46801885440024e59783e87e0
imReset <fffffreinds101@gmail.com>
2022-02-14 20:05:24 -0800

    add sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,31 @@
-import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import CoinbaseLogo from "../assets/logo.png";
+import Image from "next/image";
+import { navItems } from "../data/navItems";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const [activeIcon, setActiveIcon] = useState(navItems[0].icon);
+
+  return (
+    <Wrapper>
+      <LogoContainer>
+        <Logo>
+          <Image src={CoinbaseLogo} alt="Coinbase Logo" />
+        </Logo>
+      </LogoContainer>
+      <NavItemsContainer>
+        {navItems.map((item, index) => (
+          <NavItem key={index} onClick={() => setActiveIcon(item.title)}>
+            <NavIcon style={{ color: item.title === activeIcon && "#3773f5" }}>
+              {item.icon}
+            </NavIcon>
+            <NavTitle>{item.title}</NavTitle>
+          </NavItem>
+        ))}
+      </NavItemsContainer>
+    </Wrapper>
+  );
 }
 
 export default Sidebar;
