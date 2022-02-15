40d41bdc875712885e74d272d89759273b458631
imReset <fffffreinds101@gmail.com>
2022-02-14 19:47:20 -0800

    add stylinh

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -73,3 +73,67 @@ function Portfolio({ thirdWebTokens, sanityTokens, walletAddress }) {
     </Wrapper>
   );
 }
+
+export default Portfolio;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  padding: 2rem 1rem;
+`;
+
+const Chart = styled.div`
+  border: 1px solid #282b2f;
+  padding: 1rem 2rem;
+`;
+
+const Balance = styled.div``;
+
+const BalanceTitle = styled.div`
+  color: #8a919e;
+  font-size: 0.9rem;
+`;
+
+const BalanceValue = styled.div`
+  font-size: 1.8rem;
+  font-weight: 700;
+  margin: 0.5rem 0;
+`;
+
+const PortfolioTable = styled.div`
+  margin-top: 1rem;
+  border: 1px solid #282b2f;
+`;
+
+const Table = styled.table`
+  width: 100%;
+`;
+
+const TableRow = styled.tr`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  & > th {
+    text-align: left;
+  }
+`;
+
+const TableItem = styled.div`
+  padding: 1rem 2rem;
+`;
+
+const Divier = styled.div`
+  border-bottom: 1px solid #282b2f;
+`;
+
+const Title = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
