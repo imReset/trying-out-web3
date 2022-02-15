209f6bbdd70597b3589b8e2d9ff7a8c0ab027af0
imReset <fffffreinds101@gmail.com>
2022-02-14 19:52:17 -0800

    a

--- components/Promos.js
+++ components/Promos.js
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 function Promos() {
-  return <div>Promos</div>;
+  return (
+    <Wrapper>
+      <OfferCard>
+        <Title>Yield earned</Title>
+        <Description>Earn up to 3% APY on your crypto</Description>
+        <Placeholder />
+        <Additional style={{ fontSize: "1.5rem" }}>
+          $0.00066 <span>2.8% APY</span>
+        </Additional>
+      </OfferCard>
+      <OfferCard>
+        <Title>Learn and Earn</Title>
+        <Description>Earn up to 2.8% APY on your crypto</Description>
+        <Placeholder />
+        <Additional style={{ color: "#3773f5" }}>Verify Identity</Additional>
+      </OfferCard>
+    </Wrapper>
+  );
 }
 
 export default Promos;
