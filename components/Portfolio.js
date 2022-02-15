940b9ded765682a48ef1f5eafd1ea0499def1e96
imReset <fffffreinds101@gmail.com>
2022-02-14 19:40:36 -0800

    add portfolio

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -27,4 +27,49 @@ function Portfolio({ thirdWebTokens, sanityTokens, walletAddress }) {
 
     return calculateTotalBalance();
   }, [thirdWebTokens, sanityTokens]);
+
+  return (
+    <Wrapper>
+      <Content>
+        <Chart>
+          <div>
+            <BalanceTitle>Portfolio Balance</BalanceTitle>
+            <BalanceValue>
+              {"$"}
+              {walletBalance.toLocaleString()}
+            </BalanceValue>
+          </div>
+          <BalanceChart />
+        </Chart>
+
+        <TableItem>
+          <Title>Your Assets</Title>
+        </TableItem>
+        <Divider />
+        <Table>
+          <TableItem>
+            <TableRow>
+              <div style={{ flex: 3 }}>Name</div>
+              <div style={{ flex: 2 }}>Balance</div>
+              <div style={{ flex: 1 }}>Price</div>
+              <div style={{ flex: 1 }}>Allowcation</div>
+              <div style={{ flex: 0 }}>
+                <BsThreeDotsVertical />
+              </div>
+            </TableRow>
+          </TableItem>
+          <Divider />
+          <div>
+            {coins.map((coin, index) => (
+              <div key={index}>
+                <Coin coin={coin} />
+                <Divider />
+              </div>
+            ))}
+          </div>
+        </Table>
+        <PortfolioTable />
+      </Content>
+    </Wrapper>
+  );
 }
